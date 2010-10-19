09a6017dea18aa6e99a83990aa2de82371f7d87e
Ezequiel <exe.zaid@gmail.com>
2010-10-19 00:34:45 -0300

    add create in controller

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -4,4 +4,17 @@ class Admin::PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to admin_posts_path
+    else
+      render :action => :new
+    end
+  end
+
 end
