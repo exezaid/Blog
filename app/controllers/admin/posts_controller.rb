3c07cca6126063eaefbceff07aff18da9effe138
Ezequiel <exe.zaid@gmail.com>
2010-10-19 01:18:28 -0300

    controller destroy

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -34,4 +34,11 @@ class Admin::PostsController < ApplicationController
     end
   end
 
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_path
+  end
+
 end
